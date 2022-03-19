3ca8c0ff29f7bbace59a0c97512432fcd2d175c1
Mouhamet99 <69980134+Mouhamet99@users.noreply.github.com>
2022-03-19 16:15:53 +0000

    Add New client using event and state

--- src/App.js
+++ src/App.js
@@ -7,24 +7,52 @@ export default class App extends React.Component {
       { id: 1, nom: "Adou Karim" },
       { id: 2, nom: "Fati Diop" },
       { id: 3, nom: "Anta Mbaye" }
-    ]
+    ],
+    inputText: "",
+    compteur: 0
   };
+
   handleClcik = () => {
     console.log(this.state);
+    this.setState({ compteur: ++this.state.compteur });
   };
+
+  handleChange = (e) => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.inputText === "") return false;
+    const id = this.state.clients.length + 1;
+    const nom = this.state.inputText;
+    const clients = [...this.state.clients, { id, nom }];
+    this.setState({ clients, inputText: "" });
+  };
+
   render() {
     const clients = this.state.clients.map((client) => (
       <li key={client.id}>
         {client.nom} <button>X</button>
       </li>
     ));
+
     return (
       <div className="App">
         <h1>Liste des clients</h1>
         <ul>{clients}</ul>
-        <input placeholder="entrez votre produit" />
-        <input type="submit" value="Ajouter" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.inputText}
+            onChange={this.handleChange}
+            placeholder="entrez votre client"
+          />
+          <input type="submit" value="Ajouter" />
+        </form>
+        <hr />
         <button onClick={this.handleClcik}>Click me</button>
+        <p>{this.state.compteur}</p>
+        <p>{this.state.inputText}</p>
       </div>
     );
   }
