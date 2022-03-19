a1d72b3aa4faff2e892eeef3c11bae06d698526f
Mouhamet99 <69980134+Mouhamet99@users.noreply.github.com>
2022-03-19 14:58:13 +0000

    Display Clients From the state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    clients: [
+      { id: 1, nom: "Adou Karim" },
+      { id: 2, nom: "Fati Diop" },
+      { id: 3, nom: "Anta Mbaye" }
+    ]
+  };
+  render() {
+    const clients = this.state.clients.map((client) => (
+      <li key={client.id}>
+        {client.nom} <button>X</button>
+      </li>
+    ));
+    return (
+      <div className="App">
+        <h1>Liste des clients</h1>
+        <ul>{clients}</ul>
+        <input placeholder="entrez votre produit" />
+        <input type="submit" value="Ajouter" />
+      </div>
+    );
+  }
 }
