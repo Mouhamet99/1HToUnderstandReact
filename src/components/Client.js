0c88aac8f31ae379fbb09616eefd59ec57293b45
Mouhamet99 <69980134+Mouhamet99@users.noreply.github.com>
2022-03-19 18:25:41 +0000

    transform our fonctional class component to functional component

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
-class Client extends Component {
-  render() {
-    const client = this.props.infos;
+import React from "react";
+const Client = (props) => {
+  const client = props.infos;
 
-    return (
-      <li key={client.id}>
-        {client.nom}
-        <button onClick={() => this.props.onDelete(this.props.infos.id)}>
-          X
-        </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li key={client.id}>
+      {client.nom}
+      <button onClick={() => this.props.onDelete(this.props.infos.id)}>
+        X
+      </button>
+    </li>
+  );
+};
 export default Client;
